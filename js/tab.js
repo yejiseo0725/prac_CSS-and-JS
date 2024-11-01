0b29aea7f7084e300187157a59a0b768758bb809
YEJI SEO <yejiseo0725@gmail.com>
2024-11-02 00:04:39 +0900

    tap menu show info

--- js/tab.js
+++ js/tab.js
@@ -1,12 +1,8 @@
-버튼0 누르면 
-- 모든버튼 에 붙은 color 클래스명 제거
-- 버튼0 에 color 클래스명 추가
-- 모든div 에 붙은 show 클래스명 제거
-- div0 에 show 클래스명 추가
-
 // === querySelectorAll('.tab-button')[0]
-$('.tab-button').eq(0).on('click', function() {
+$(".tab-button")
+  .eq(0)
+  .on("click", function () {
     $(this).addClass("color");
-    $('.tab-button').eq(1).removeClass("color");
-    $('.tab-button').eq(2).removeClass("color");
-});
+    $(".tab-button").eq(1).removeClass("color");
+    $(".tab-button").eq(2).removeClass("color");
+  });
