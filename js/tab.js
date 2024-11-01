219c24fe83f1c4358a833e8fe9b9268403c6e00b
YEJI SEO <yejiseo0725@gmail.com>
2024-11-01 23:37:03 +0900

    tab menu

--- js/tab.js
+++ js/tab.js
@@ -6,5 +6,7 @@
 
 // === querySelectorAll('.tab-button')[0]
 $('.tab-button').eq(0).on('click', function() {
-
+    $(this).addClass("color");
+    $('.tab-button').eq(1).removeClass("color");
+    $('.tab-button').eq(2).removeClass("color");
 });
